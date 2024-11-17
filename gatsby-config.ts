0254bbec99d71a083da4854b9777f768eebe3d1e
MR Ferry <ferferfx@gmail.com>
2024-11-17 20:42:21 +0700

    fix: try remove offline plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -87,7 +87,8 @@ const config: GatsbyConfig = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    "gatsby-plugin-offline",
+    "gatsby-plugin-remove-serviceworker",
+    // "gatsby-plugin-offline",
   ],
 };
 
