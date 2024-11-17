da6c84c3b6a876bb1fa3994315ebfe9d1d7dd9af
MR Ferry <ferferfx@gmail.com>
2024-11-17 18:41:57 +0700

    refactor: twitter card

--- src/component/Seo.tsx
+++ src/component/Seo.tsx
@@ -12,6 +12,7 @@ import { data } from "../Data";
  ************************/
 
 export default function Seo() {
+  const cardImagePath = `ferry-suhandri-large2.png`;
   return (
     <>
       <html lang={data.lang} />
@@ -22,14 +23,14 @@ export default function Seo() {
       <meta name="og:type" content={`website`} />
       <meta name="og:site_name" content={data.siteConfig.realName} />
       <meta name="og:url" content={data.siteConfig.metaUrl} />
-      <meta name="og:image" content={`${data.siteConfig.metaUrl}/ferry-suhandri-large2.jpg`} />
+      <meta name="og:image" content={`${data.siteConfig.metaUrl}/${cardImagePath}`} />
       <meta name="og:image:type" content={`image/jpeg`} />
       <meta name="og:image:width" content={`338`} />
       <meta name="og:image:height" content={`463`} />
       <meta name="twitter:card" content={`summary`} />
       <meta name="twitter:creator" content={data.siteConfig.realName} />
       <meta name="twitter:title" content={data.siteConfig.realName} />
-      <meta name="twitter:image" content={`${data.siteConfig.metaUrl}/ferry-suhandri-large2.jpg`} />
+      <meta name="twitter:image" content={`${data.siteConfig.metaUrl}/${cardImagePath}`} />
       <meta name="twitter:description" content={data.siteConfig.description} />
       <meta name="fb:app_id" content={`708906762920155`} />
       <meta name="google-site-verification" content={`6RziIE_ZhYMRcSOJtYpoYAYsnqTn0nUbLZS3nXgC2Hw`} />
